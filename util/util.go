f5482ba2112a6ef10f6d148e62393f31794fc8ab
qifenggang <fenggang.qi@ymm56.com>
2018-11-20 20:24:27 +0800

    修改

--- util/util.go
+++ util/util.go
@@ -41,14 +41,14 @@ func HandleContent(content string) string {
 	}
 
 	//content = strings.Replace(content, "][", " ", -1)
-    tmpcontent := strings.Split(content,"][")
-    fmt.Println(tmpcontent)
-    alertcontent := strings.Split(tmpcontent[4]," ")
-    fmt.Println(alertcontent)
-    alertstring := strings.Join(alertcontent[0:len(alertcontent)-4], " ")
-    timecontent := strings.Split(tmpcontent[5]," ")
-    wantcontent :=  "============falcon============" + "\n告警级别 : " + tmpcontent[0] + "\n告警状态 : " +  tmpcontent[1] + "\n主机名   : " + tmpcontent[2] + "\n告警内容 : " +  alertstring + "\n监控项   : "  + alertcontent[len(alertcontent)-3] + "\n监控tag  : " + alertcontent[len(alertcontent)-2] + "\n监控值   : " + alertcontent[len(alertcontent)-1] + "\n告警次数 : " + timecontent[0] + "\n告警时间 : " + timecontent[1] +" "+ timecontent[2]
-    fmt.Println(wantcontent)
+        tmpcontent := strings.Split(content,"][")
+        fmt.Println(tmpcontent)
+        alertcontent := strings.Split(tmpcontent[4]," ")
+        fmt.Println(alertcontent)
+        alertstring := strings.Join(alertcontent[0:len(alertcontent)-4], " ")
+        timecontent := strings.Split(tmpcontent[5]," ")
+        wantcontent :=  "============falcon============" + "\n告警级别 : " + tmpcontent[0] + "\n告警状态 : " +  tmpcontent[1] + "\n主机名   : " + tmpcontent[2] + "\n告警内容 : " +  alertstring + "\n监控项   : "  + alertcontent[len(alertcontent)-3] + "\n监控tag  : " + alertcontent[len(alertcontent)-2] + "\n监控值   : " + alertcontent[len(alertcontent)-1] + "\n告警次数 : " + timecontent[0] + "\n告警时间 : " + timecontent[1] +" "+ timecontent[2]
+        fmt.Println(wantcontent)
 
 
 	return wantcontent
